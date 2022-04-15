f93b24f793a7bea9315a1b535b84c0453048939c
Kenneth Imade <kenneth.imade@yahoo.com>
2022-04-15 13:58:15 +0100

    designed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import "../styles/Header.css"
 
 class Header extends Component {
 
     render() {
         return (
-            <p>Header goes here</p>
+            <header className="header">
+                <p className="header--title">CV Generator</p>
+            </header>
         )
     }
 };
