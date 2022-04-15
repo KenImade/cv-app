b27e486eb176ef495374d790d934f261f5f84dfb
Kenneth Imade <kenneth.imade@yahoo.com>
2022-04-15 15:31:00 +0100

    personal details section functionality

--- src/components/CV.js
+++ src/components/CV.js
@@ -5,11 +5,43 @@ import WorkExperience from "./WorkExperience";
 
 class CV extends Component {
 
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            title: "",
+            address: "",
+            phoneNumber: "",
+            email: "",
+            description: ""
+        };
+
+        this.handleInput = this.handleInput.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
+    handleInput(event) {
+        console.log(event.target.name)
+        console.log(event.target.value)
+        const {name, value} = event.target
+        this.setState(() => {
+            return {
+                [name]: value 
+            }
+        })
+    }
+
     render() {
         return (
             <main>
-                <Education />
-                <PersonalDetails />
+                <PersonalDetails info={this.state} getInput={this.handleInput} />
+                <Education  />
                 <WorkExperience />
             </main>
         )
--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,10 +1,65 @@
 import React, { Component } from "react";
+import "../styles/PersonalDetails.css"
 
 class PersonalDetails extends Component{
 
     render() {
+        const {firstName, lastName, title, address, phoneNumber, email, description} = this.props.info
+        // console.log(this.props.info)
         return (
-            <h4>Personal Details</h4>
+            <section className="personal--details">
+                <h4 className="personal--detail--title">Personal Details</h4>
+                <div className="personal--details--form">
+                    <input 
+                        type="text"
+                        placeholder="First name"
+                        name="firstName"
+                        onChange={this.props.getInput}
+                        value={firstName}
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Last name"
+                        name="lastName"
+                        onChange={this.props.getInput}
+                        value={lastName}
+                    />
+                    <input
+                        type="text" 
+                        placeholder="Title"
+                        name="title"
+                        onChange={this.props.getInput}
+                        value={title} 
+                    />
+                    <input
+                        type="text" 
+                        placeholder="Address"
+                        name="address"
+                        onChange={this.props.getInput}
+                        value={address}
+                    />
+                    <input
+                        type="text" 
+                        placeholder="Phone number"
+                        name="phoneNumber"
+                        onChange={this.props.getInput}
+                        value={phoneNumber}
+                    />
+                    <input 
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        onChange={this.props.getInput}
+                        value={email}
+                    />
+                    <textarea
+                        placeholder="Description"
+                        name="description"
+                        onChange={this.props.getInput}
+                        value={description}
+                    ></textarea>
+                </div>
+            </section>
         )
     }
 }
